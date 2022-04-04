4a6bfc20c26cf18f991e9843b73eb50545706e3f
Jase Pellerin <jasepellerin@gmail.com>
2022-04-03 18:16:52 -0700

    First attempt at custom shield

---
+++ config/unit01.h
@@ -0,0 +1,13 @@
+
+#define XXX &none
+#define ___ &trans
+
+#define BASE 0
+#define NAV 1
+#define NUM 2
+
+#define SK(KEY) &nk KEY KEY
+#define MO(LAYER) &mo_tap LAYER F24
+
+// Keep sticky keys active for a long time so they effectively do not time out
+#define STICKY_KEY_TIMEOUT 60000
